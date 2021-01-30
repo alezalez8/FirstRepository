2f38a02d4435516c30415847c3bdbbfea42911e5
alezalez8 <5446943@ukr.net>
2021-01-30 15:28:55 +0200

    first change in src/aop/aspects/ClassForGit.java

--- src/aop/aspects/ClassForGit.java
+++ src/aop/aspects/ClassForGit.java
@@ -1,4 +1,31 @@
 package aop.aspects;
 
 public class ClassForGit {
+    private String myName;
+    private String yourName;
+    private int myAge;
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+
+    public String getYourName() {
+        return yourName;
+    }
+
+    public void setYourName(String yourName) {
+        this.yourName = yourName;
+    }
+
+    public int getMyAge() {
+        return myAge;
+    }
+
+    public void setMyAge(int myAge) {
+        this.myAge = myAge;
+    }
 }
