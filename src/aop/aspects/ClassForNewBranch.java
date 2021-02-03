76e60085def1687c381292b4efadf9171a8fed02
alezalez8 <5446943@ukr.net>
2021-02-03 21:04:45 +0200

    second save into branch add-feature1

--- src/aop/aspects/ClassForNewBranch.java
+++ src/aop/aspects/ClassForNewBranch.java
@@ -2,4 +2,17 @@ package aop.aspects;
 
 public class ClassForNewBranch {
     // new branch add-feature1 is created
+
+
+    private static int a = 10;
+    private static int b = 20;
+    private static int c;
+
+
+    public static void main(String[] args) {
+
+        System.out.println(a + b);
+
+
+    }
 }
